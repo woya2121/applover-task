a03297b130ddfd5184f4b58995bd007e93b61f8a
woya2121 <woya2121@gmail.com>
2024-09-09 16:27:17 +0200

    Clean up

--- api/models.py
+++ api/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 
 
-
-
 class Book(models.Model):
     serial_number = models.CharField(max_length=6)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     is_borrowed = models.BooleanField(default=False)
-    # clients = models.ForeignKey(
-    #     LibraryClient, on_delete=models.CASCADE, blank=True, null=True
-    # )
 
     def __str__(self) -> str:
         return self.title
